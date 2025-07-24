03094c8fd842c098cfb9db4c7433466d3e24cfcc
trentg@ppai.org <trentg@ppai.org>
2025-07-24 16:18:49 -0500

    Lesson 03 - Colors and Styles

--- SpectreDemo/Program.cs
+++ SpectreDemo/Program.cs
@@ -3,9 +3,21 @@
 AnsiConsole.Clear();
 
 // Lesson 02 Initial Setup
-AnsiConsole.MarkupLine("[red bold]Hello World[/]");
-AnsiConsole.MarkupLine("Hello World");
-AnsiConsole.MarkupLine("[slowblink]Hello World[/]");
+// AnsiConsole.MarkupLine("[red bold]Hello World[/]");
+// AnsiConsole.MarkupLine("Hello World");
+// AnsiConsole.MarkupLine("[slowblink]Hello World[/]");
+
+//Lesson 03 - Colors and Styles
+AnsiConsole.MarkupLine("[red on white bold]This is the inline markup[/]");
+
+Style danger = new(
+    foreground: Color.Red,
+    background: Color.White,
+    Decoration.Bold | Decoration.Italic | Decoration.RapidBlink);
+
+//AnsiConsole.Write(new Markup("Danger Text from Style\n", danger));
+AnsiConsole.Write(new Markup("Danger Text from Style", danger));
+AnsiConsole.WriteLine(" and more...");
 
 AnsiConsole.MarkupLine("");
 
