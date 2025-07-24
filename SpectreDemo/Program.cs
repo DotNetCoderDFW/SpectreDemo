8d074c9d4af94fdf6944219687becb9b64386922
trentg@ppai.org <trentg@ppai.org>
2025-07-24 16:15:10 -0500

    Lesson 02

--- SpectreDemo/Program.cs
+++ SpectreDemo/Program.cs
@@ -2,7 +2,10 @@
 
 AnsiConsole.Clear();
 
-
+// Lesson 02 Initial Setup
+AnsiConsole.MarkupLine("[red bold]Hello World[/]");
+AnsiConsole.MarkupLine("Hello World");
+AnsiConsole.MarkupLine("[slowblink]Hello World[/]");
 
 AnsiConsole.MarkupLine("");
 
