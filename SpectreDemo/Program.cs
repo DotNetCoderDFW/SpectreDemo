3ee82ac29b9695822e9a32e843c0ca5331bcf12c
trentg@ppai.org <trentg@ppai.org>
2025-07-24 16:20:37 -0500

    Lesson 04 - Text Prompts

--- SpectreDemo/Program.cs
+++ SpectreDemo/Program.cs
@@ -8,16 +8,36 @@ AnsiConsole.Clear();
 // AnsiConsole.MarkupLine("[slowblink]Hello World[/]");
 
 //Lesson 03 - Colors and Styles
-AnsiConsole.MarkupLine("[red on white bold]This is the inline markup[/]");
+// AnsiConsole.MarkupLine("[red on white bold]This is the inline markup[/]");
+//
+// Style danger = new(
+//     foreground: Color.Red,
+//     background: Color.White,
+//     Decoration.Bold | Decoration.Italic | Decoration.RapidBlink);
+//
+// //AnsiConsole.Write(new Markup("Danger Text from Style\n", danger));
+// AnsiConsole.Write(new Markup("Danger Text from Style", danger));
+// AnsiConsole.WriteLine(" and more...");
 
-Style danger = new(
-    foreground: Color.Red,
-    background: Color.White,
-    Decoration.Bold | Decoration.Italic | Decoration.RapidBlink);
-
-//AnsiConsole.Write(new Markup("Danger Text from Style\n", danger));
-AnsiConsole.Write(new Markup("Danger Text from Style", danger));
-AnsiConsole.WriteLine(" and more...");
+// Lesson 04 - Text Prompts
+//int age = AnsiConsole.Ask<int>("What is you age?");
+//bool isHappy = AnsiConsole.Ask<bool>("Are you happy?");
+int age = AnsiConsole.Prompt(
+ new TextPrompt<int>("What is your age?")
+     .Validate((x) => x switch
+     {
+         < 0 => ValidationResult.Error("You were not born yet"),
+         > 120 => ValidationResult.Error("You are too old"),
+         _ => ValidationResult.Success()
+     })
+);
+string happyText = AnsiConsole.Prompt(
+ new TextPrompt<string>("Are you happy")
+     .AddChoice("yes")
+     .AddChoice("no")
+     .DefaultValue("yes")
+);
+AnsiConsole.MarkupLine($"Happy: {happyText}\nAge: {age}\n");
 
 AnsiConsole.MarkupLine("");
 
